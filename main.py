0e2d0172d131a8ae0d1d461d713182324d60bf6b
Larissa Lemos Afonso <126449248+LarissaLAfonso@users.noreply.github.com>
2023-09-18 08:39:04 -0300

    function that calculates diff between 2 dates

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 18 07:46:11 2023
+import datecalculator
+import userchoice
 
-@author: b51055
-"""
+def calculatediff(dates):
+    """
+    Calculates the amount of days between two dates.
+
+    Parameters
+    ----------
+    dates : string
+        Inputed dates.
+
+    Returns
+    -------
+    None.
+
+    """
+    date_1, date_2 = dates.split(" - ")
+    date_1 = datecalculator.parse_date_string(date_1)
+    date_2 = datecalculator.parse_date_string(date_2)
+    print(datecalculator.calculate_days_between_dates(date_1, date_2))
+
+user_input = userchoice.type_of_input()
+
+if user_input == "Q":
+    print("You quit.")
+elif user_input == "C":
+    dates = input("Input dates.")
+    calculatediff(dates)
+elif user_input == "F":
+    file_name = input("Input file path.")
+    with open(file_name, "r") as file:
+        dates = file.read()
+        calculatediff(dates)
+    
 
