e55461e17f886793cdd3f60ec93071ad5826fb92
Larissa Lemos Afonso <126449248+LarissaLAfonso@users.noreply.github.com>
2023-09-18 08:40:08 -0300

    turn input into uppercase

--- userchoice.py
+++ userchoice.py
@@ -13,7 +13,7 @@ def type_of_input():
     """
     
     user_choice = input("What is the chosen type of input?\nIf file, type 'F',\n if console input type 'C',\n if you want to quite type 'Q'.")
-    
+    user_choice = user_choice.upper()
         
     try:
         if user_choice == "F" or user_choice == "Q" or user_choice == "C":
