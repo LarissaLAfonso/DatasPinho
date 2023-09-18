42d94bdff286c89b275b8db716993622d5a65095
Larissa Lemos Afonso <126449248+LarissaLAfonso@users.noreply.github.com>
2023-09-18 08:06:30 -0300

    verify user input

--- userchoice.py
+++ userchoice.py
@@ -1,2 +1,24 @@
 def TypeOfInput():
-    user_choice = input("")
+    """
+    Asks the user if they want to input the dates in a file, in the console or 
+    if they want to quit.
+
+    Returns
+    -------
+    The type of user input.
+
+    """
+    
+    user_choice = input("What is the chosen type of input?\nIf file, type 'F',\n if console input type 'C',\n if you want to quite type 'Q'.")
+    
+        
+    try:
+        if user_choice == "F" or user_choice == "Q" or user_choice == "C":
+            return user_choice
+        else:
+            raise ValueError
+    except:
+        print("Your input is invalid. Please type one of the designated inputs.")
+        TypeOfInput()
+
+        
